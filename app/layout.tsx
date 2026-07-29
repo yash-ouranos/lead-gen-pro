f864aa0db4fed00caaa4513b020295812db89e01
Yash Kevadiya <yash.kevadiya@ouranostech.com>
2026-07-29 16:29:11 +0530

    feat: enable smooth scrolling globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
  children: React.ReactNode;
 }>) {
  return (
- <html lang="en">
+ <html lang="en" className="scroll-smooth">
  <body className={`${ibmPlexSans.className} ${ibmPlexSans.variable} ${ibmPlexMono.variable}`}>
  <Providers>
  {children}
